2353d5b458f49cd1accf0bd722b468b37fd0ae03
veronikakoval96 <veronikakoval0009@gmail.com>
2025-04-21 22:14:34 +0200

    cambios

--- main.js
+++
@@ -1,5 +0,0 @@
-import "./src/header/header.js";
-import "./src/body/body.js";
-import "./src/components/funciones.js";
-import "./src/main.scss";
-
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "./header/header.js";
+import "./body/body.js";
+import "./components/funciones.js";
+import "./main.scss";
+
